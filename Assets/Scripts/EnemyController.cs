4f59da8a47283d283789cdc8e4173443d01a553d
phuduy2610 <phuduy2610@gmail.com>
2021-09-29 22:28:57 +0700

    - Animation của robot và ruby

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -11,12 +11,15 @@ public class EnemyController : MonoBehaviour
     Rigidbody2D rigid_body;
     float timer;
     int direction = 1;
+
+    Animator animator;
     
     // Start is called before the first frame update
     void Start()
     {
         rigid_body = GetComponent<Rigidbody2D>();
         timer = changeTime;
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -28,6 +31,7 @@ public class EnemyController : MonoBehaviour
             direction = -direction;
             timer = changeTime;
         }
+
     }
     
     void FixedUpdate()
@@ -36,11 +40,15 @@ public class EnemyController : MonoBehaviour
         
         if (vertical)
         {
-            position.y = position.y + Time.deltaTime * speed * direction;;
+            position.y = position.y + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X",0);
+            animator.SetFloat("Move Y",direction);
         }
         else
         {
-            position.x = position.x + Time.deltaTime * speed * direction;;
+            position.x = position.x + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X",direction);
+            animator.SetFloat("Move Y",0);
         }
         
         rigid_body.MovePosition(position);
--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -19,11 +19,15 @@ public class RubyController : MonoBehaviour
     Rigidbody2D rigid_body;
     float horizontal;
     float vertical;
+
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1,0);
     // Start is called before the first frame update
     void Start()
     {
         rigid_body = GetComponent<Rigidbody2D>();
         _health = 2;
+        animator = GetComponent<Animator>();
         Debug.Log(_health);
     }
 
@@ -32,6 +36,14 @@ public class RubyController : MonoBehaviour
         vertical = Input.GetAxis("Vertical");
         horizontal = Input.GetAxis("Horizontal"); // Lấy input  trái phải
 
+        Vector2 move = new Vector2(horizontal,vertical);
+        if(!Mathf.Approximately(move.x,0.0f)||!Mathf.Approximately(move.y,0.0f)){
+            lookDirection.Set(move.x,move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look X",lookDirection.x);
+        animator.SetFloat("Look Y",lookDirection.y);
+        animator.SetFloat("Speed",move.magnitude);
         if(isInvincible){
             invincibleTimer -= Time.deltaTime;
             if(invincibleTimer < 0){
@@ -49,6 +61,7 @@ public class RubyController : MonoBehaviour
 
     public void ChangeHealth(int amount){
         if(amount<0){
+            animator.SetTrigger("Hit");
             if(isInvincible){
                 return;
             }
